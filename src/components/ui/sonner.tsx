93a7134ec65cb028f40461748bfe827ad8581709
Victor <93287475+Neoznzoe@users.noreply.github.com>
2025-09-02 17:00:58 +0200

    Limit toast size and duration

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -10,11 +10,15 @@ const Toaster = ({ ...props }: ToasterProps) => {
     <Sonner
       theme={theme as ToasterProps['theme']}
       className="toaster group"
+      closeButton
       toastOptions={{
+        duration: 6000,
         classNames: {
           toast:
-            'group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg',
-          description: 'group-[.toast]:text-muted-foreground',
+            'group toast max-w-[250px] group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg',
+          title: 'truncate',
+          description:
+            'group-[.toast]:text-muted-foreground truncate',
           actionButton:
             'group-[.toast]:bg-primary group-[.toast]:text-primary-foreground',
           cancelButton:
