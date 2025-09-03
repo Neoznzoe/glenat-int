9b2d1efa594589e4cd393b7a02611ee42d05d5f5
Victor <93287475+Neoznzoe@users.noreply.github.com>
2025-09-03 16:20:41 +0200

    Add Mobilités and Bureautique service cards

--- src/pages/services/Services.tsx
+++ src/pages/services/Services.tsx
@@ -25,101 +25,136 @@ export function Services() {
 
   const cards: { title: string; links: LinkItem[] }[] = [
     {
-        title: 'G\u00e9n\u00e9ral',
-        links: [
-          {
-            label: 'Documentations, notices, proc\u00e9dures',
-            type: 'header',
-          },
-          { label: 'Informatique / charte informatique', type: 'text', separator: false },
-          { label: 'Charte informatique', href: '#', type: 'link', separator: true },
-        ],
-      },
-      {
-        title: 'Mat\u00e9riel, r\u00e9seaux et acc\u00e8s internet',
-        links: [
-          { label: 'Formulaire de demande', type: 'header' },
-          {
-            label: 'Informatique / charte informatique',
-            type: 'text',
-            separator: true,
-          },
-          { label: 'Demande investissement', href: '#', type: 'link', separator: false },
-          { label: 'Demande interventions', href: '#', type: 'link', separator: false },
-        ],
-      },
-      {
-        title: 'Site internet et intranet',
-        links: [
-          { label: 'Contact utile', type: 'header' },
-          { label: 'Matthieu Nicolas', type: 'text', separator: false },
-          {
-            label: 'Documentations, notices, proc\u00e9dures',
-            type: 'header',
-            separator: true,
-          },
-          { label: 'Informatique/internet', type: 'text', separator: false },
-          {
-            label: 'Site internet - mentions obligatoires',
-            href: '#',
-            type: 'link',
-            separator: false,
-          },
-          {
-            label: 'Mailnblack - connexion compte Office 365',
-            href: '#',
-            type: 'link',
-            separator: false,
-          },
-          { label: 'Formulaire de demande', type: 'header', separator: true },
-          {
-            label: "Demande d'interventions informatique",
-            href: '#',
-            type: 'link',
-            separator: false,
-          },
-        ],
-      },
-      {
-        title: 'Nautilus',
-        links: [
-          { label: 'Contact utiles', type: 'header' },
-          { label: 'Catherine Jullin', type: 'text', separator: false },
-          {
-            label: 'Documentations, notices, proc\u00e9dures',
-            type: 'header',
-            separator: true,
-          },
-          { label: 'Proc\u00e9dures internes / note de frais', type: 'text', separator: false },
-          {
-            label: 'Proc\u00e9dure de commandes voyages',
-            href: '#',
-            type: 'link',
-            separator: false,
-          },
-          { label: 'Horaires TGV 2025', href: '#', type: 'link', separator: false },
-          { label: 'Manuel utilisateurs Notilus', href: '#', type: 'link', separator: false },
-          { label: 'Place de d\u00e9placement multimodal', href: '#', type: 'link', separator: false },
-          {
-            label: 'Note de demande d\u00e9placement professionnel',
-            href: '#',
-            type: 'link',
-            separator: false,
-          },
-          {
-            label: 'Notilus 2022 - Manuel utilisateurs',
-            href: '#',
-            type: 'link',
-            separator: false,
-          },
-          {
-            label: 'Notilus 2022 - proc\u00e9dures simplifi\u00e9e',
-            href: '#',
-            type: 'link',
-            separator: false,
-          },
-        ],
-      },
+      title: 'Général',
+      links: [
+        { label: 'Documentations, notices, procédures', type: 'header' },
+        { label: 'Informatique / charte informatique', type: 'text', separator: false },
+        { label: 'Charte informatique', href: '#', type: 'link', separator: true },
+      ],
+    },
+    {
+      title: 'Matériel, réseaux et accès internet',
+      links: [
+        { label: 'Formulaire de demande', type: 'header' },
+        { label: 'Informatique / charte informatique', type: 'text', separator: true },
+        { label: 'Demande investissement', href: '#', type: 'link', separator: false },
+        { label: 'Demande interventions', href: '#', type: 'link', separator: false },
+      ],
+    },
+    {
+      title: 'Site internet et intranet',
+      links: [
+        { label: 'Contact utile', type: 'header' },
+        { label: 'Matthieu Nicolas', type: 'text', separator: false },
+        { label: 'Documentations, notices, procédures', type: 'header', separator: true },
+        { label: 'Informatique/internet', type: 'text', separator: false },
+        { label: 'Site internet - mentions obligatoires', href: '#', type: 'link', separator: false },
+        { label: 'Mailnblack - connexion compte Office 365', href: '#', type: 'link', separator: false },
+        { label: 'Formulaire de demande', type: 'header', separator: true },
+        { label: "Demande d'interventions informatique", href: '#', type: 'link', separator: false },
+      ],
+    },
+    {
+      title: 'Nautilus',
+      links: [
+        { label: 'Contact utiles', type: 'header' },
+        { label: 'Catherine Jullin', type: 'text', separator: false },
+        { label: 'Documentations, notices, procédures', type: 'header', separator: true },
+        { label: 'Procédures internes / note de frais', type: 'text', separator: false },
+        { label: 'Procédure de commandes voyages', href: '#', type: 'link', separator: false },
+        { label: 'Horaires TGV 2025', href: '#', type: 'link', separator: false },
+        { label: 'Manuel utilisateurs Notilus', href: '#', type: 'link', separator: false },
+        { label: 'Place de déplacement multimodal', href: '#', type: 'link', separator: false },
+        { label: 'Note de demande déplacement professionnel', href: '#', type: 'link', separator: false },
+        { label: 'Notilus 2022 - Manuel utilisateurs', href: '#', type: 'link', separator: false },
+        { label: 'Notilus 2022 - procédures simplifiée', href: '#', type: 'link', separator: false },
+      ],
+    },
+    {
+      title: 'Mobilités',
+      links: [
+        { label: 'Documentations, notices, procédures', type: 'header' },
+        { label: 'Informatique / mobilités', type: 'text', separator: false },
+        {
+          label: 'Comment désactiver le mode push de son iPhone',
+          href: '#',
+          type: 'link',
+          separator: true,
+        },
+        {
+          label: 'Les sites internets glénat sur mobile',
+          href: '#',
+          type: 'link',
+          separator: false,
+        },
+        {
+          label: "Recevoir une alerte glénat'matin sur son iPhone",
+          href: '#',
+          type: 'link',
+          separator: false,
+        },
+        { label: 'Formulaire de demande', type: 'header', separator: true },
+        { label: "Demande d'interventions informatique", href: '#', type: 'link', separator: false },
+      ],
+    },
+    {
+      title: 'Bureautique',
+      links: [
+        { label: 'Documentations, notices, procédures', type: 'header' },
+        { label: 'Informatique / Charte informatique', type: 'text', separator: false },
+        {
+          label: 'Se connecter aux calendriers partagés',
+          href: '#',
+          type: 'link',
+          separator: true,
+        },
+        { label: "Documentation Glénat'Fée", href: '#', type: 'link', separator: false },
+        {
+          label: 'Réservation salle de reunion',
+          href: '#',
+          type: 'link',
+          separator: false,
+        },
+        { label: 'Gestion absence via OWA', href: '#', type: 'link', separator: false },
+        { label: 'Publipostage', href: '#', type: 'link', separator: false },
+        {
+          label: 'Outlook 2011 mac Calendrier  partagé',
+          href: '#',
+          type: 'link',
+          separator: false,
+        },
+        {
+          label: 'Office 365 OWA Calendrier partagé',
+          href: '#',
+          type: 'link',
+          separator: false,
+        },
+        { label: 'Présentation de intranet', href: '#', type: 'link', separator: false },
+        { label: 'Présentation du réseau Informatique', href: '#', type: 'link', separator: false },
+        {
+          label: 'Howto - Visio - Réservation d’une salle',
+          href: '#',
+          type: 'link',
+          separator: false,
+        },
+        {
+          label: 'Demande de disponibilité dans outlook',
+          href: '#',
+          type: 'link',
+          separator: false,
+        },
+        { label: 'Procédures internes / note de frais', type: 'header', separator: true },
+        { label: 'Manuel utilisateurs Notilus', href: '#', type: 'link', separator: false },
+        {
+          label: 'Plan de déplacements multimodal',
+          href: '#',
+          type: 'link',
+          separator: false,
+        },
+        { label: 'Note demande de déplacement', href: '#', type: 'link', separator: false },
+        { label: 'Conditions relatives aux frais', href: '#', type: 'link', separator: false },
+      ],
+    },
   ];
 
   return (
